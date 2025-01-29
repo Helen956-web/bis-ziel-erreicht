e60681815be5ad4484002f78fafe517f16d54969
Helen956-web <helen.mueller@thg-hn.de>
2025-01-29 10:28:11 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,16 @@
+function biszielerreicht (): any {
+    if (biszielerreicht()) {
+        return true
+    } else {
+        return false
+    }
+}
+if (true) {
+    motors.motorCommand(MotorCommand.Break)
+}
+basic.setLedColor(0x00ff00)
+basic.forever(function () {
+    if (false) {
+        basic.setLedColor(0xff0000)
+    }
+})
